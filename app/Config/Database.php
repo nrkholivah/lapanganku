360ece3ad816c1d72af41aa7cc2458c5611bed17
PEMWEB1 <nurkholivah1701@gmail.com>
2025-07-08 12:00:43 +0700

    add dompdf

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'lapanganku',
+        'database'     => 'booking_lapangan',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
